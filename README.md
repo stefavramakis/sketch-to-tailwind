08aee34d67388c290f165d2dab5205a8b3895993
stefavramakis <85790489+stefavramakis@users.noreply.github.com>
2021-07-05 22:56:20 +0300

    setup tailwind from npm

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # sketch-to-tailwind
+
+## Build CSS
+
+```
+npm run build
+```
+
+or
+
+```
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: {
+      red: "#bf0a30",
+      blue: "#002868",
+      white: "#fff",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
